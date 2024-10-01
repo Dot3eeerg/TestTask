539b7f5c38aba6af9f48ef10f16101b556eca669
Ivan Danchenko <danchenko.ivan@inbox.ru>
2024-10-01 16:19:14 +0700

    update propertie

--- TestTask/Models/Circle.cs
+++ TestTask/Models/Circle.cs
@@ -4,15 +4,15 @@ namespace TestTask.Models;
 
 public class Circle : IFigure
 {
-    private readonly double _radius;
+    public readonly double Radius;
 
     public Circle(double radius)
     {
         if (radius <= 0)
             throw new ArgumentException("Radius must be greater than zero");
         
-        _radius = radius;
+        Radius = radius;
     }
 
-    public double CalculateArea() => Math.PI * _radius * _radius;
+    public double CalculateArea() => Math.PI * Radius * Radius;
 }
